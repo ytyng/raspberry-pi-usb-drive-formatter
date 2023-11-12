4aa81a36cb54463d5cbffb1c03bb2f22c530ebce
ytyng <ytyng@live.jp>
2023-11-12 21:30:54 +0900

    filter usb disk.

--- app/on-usb-plugged-in.sh
+++ app/on-usb-plugged-in.sh
@@ -3,19 +3,31 @@
 # This script is run when a USB device is plugged in.
 
 LOG_FILE=/var/log/usb-storage-formatter/usb-storage-formatter.log
+LOG_DIR=$(dirname $LOG_FILE)
 
-if [ ! -d "$LOG_DIR" ]; then
-  mkdir -p "$LOG_DIR"
+if [ ! -d "${LOG_DIR}" ]; then
+  mkdir -p "${LOG_DIR}"
+fi
+
+# echo "DEVTYPE=${DEVTYPE}, ID_USB_DRIVER=${ID_USB_DRIVER}" >> ${LOG_FILE}
+
+if [ "${DEVTYPE}" != "disk" ]; then
+  echo "[$(date +%Y-%m-%d\ %H:%M:%S)] DEVTYPE (${DEVTYPE}) is not disk. Skipping." >> ${LOG_FILE}
+  exit 0
+fi
+
+if [ "${ID_USB_DRIVER}" != "usb-storage" ]; then
+  echo "[$(date +%Y-%m-%d\ %H:%M:%S)] ID_USB_DRIVER (${ID_USB_DRIVER}) is not usb-storage. Skipping." >> ${LOG_FILE}
+  exit 0
 fi
 
 if [ $$ -ne $(pgrep -fo "$0") ]; then
   echo "[$(date +%Y-%m-%d\ %H:%M:%S)] $$: $0 is already running. Exiting." >> ${LOG_FILE}
   exit 1
 fi
-# DEVNAME=/dev/sda
-# DEVTYPE=disk
-cd $(dirname $0)
 
+cd $(dirname $0)
+echo "[$(date +%Y-%m-%d\ %H:%M:%S)] start log.sh" >> ${LOG_FILE}
 ./log.sh >> ${LOG_FILE} 2>&1
 
 # ./format-usb-storage.sh >> ${LOG_FILE} 2>&1
